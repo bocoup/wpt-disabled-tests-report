adf1905aed890aa5a2caaf269902f88076d380f1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-28 11:53:09 +0000

    Deploy bocoup/wpt-disabled-tests-report to github.com/bocoup/wpt-disabled-tests-report.git:gh-pages

---
+++ static/get-authors.js
@@ -0,0 +1,27 @@
+async function getAuthors(path) {
+  const response = await fetch("https://api.github.com/repos/web-platform-tests/wpt/commits?path=" + encodeURIComponent(path));
+  const commits = await response.json();
+  let authors = [];
+  for (const commit of commits) {
+    const author = commit.author && commit.author.login;
+    if (author && authors.indexOf(author) == -1) {
+      authors.push(author);
+    }
+  }
+  return authors;
+}
+addEventListener('click', async function(e) {
+  const target = e.target;
+  if (target instanceof HTMLAnchorElement && target.textContent === "New issue") {
+    if (e.ctrlKey || e.metaKey || e.button != 0 ||
+        target.classList.contains('has-authors')) {
+      return true;
+    }
+    e.preventDefault();
+    const path = target.parentNode.parentNode.firstElementChild.textContent;
+    const authors = await getAuthors(path);
+    target.href = target.href.replace("%40zcorpan", "%40" + authors.join(" %40"));
+    target.classList.add('has-authors');
+    target.click();
+  }
+}, false);
